283508f85875b412581a2cc9dc2d7f7809a8a51e
Kassie Sanisclaws <bytecbdeveloping@gmail.com>
2021-09-16 15:08:53 -0400

    Eight Commit

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -13,85 +13,147 @@ export default function skills() {
 
 
             <div className="skills-section-content">
-                       <div className="skills-content1">
-                          
-                          
-                          <div className="skills-card1">
-                              <p>Card1</p>
-                          </div>
+                       <div className="skills-content1">    
+                          <div className="skills-card1"> <p>Card1</p> </div>
 
-                          <div className="skills-card2">
-                              <p>Card2</p>
-                          </div>
+                          <div className="skills-card2"> <p>Card2</p> </div>
 
-                          <div className="skills-card3">
-                              <p>Card3</p>
-                          </div>
+                          <div className="skills-card3"> <p>Card3</p> </div>
 
-                          <div className="skills-card4">
-                              <p>Card4</p>
-                          </div>
+                          <div className="skills-card4"> <p>Card4</p> </div>
 
-                          <div className="skills-card5">
-                              <p>Card1</p>
-                          </div>
+                          <div className="skills-card5"> <p>Card1</p> </div>
 
-                          <div className="skills-card6">
-                              <p>Card2</p>
-                          </div>
+                          <div className="skills-card6"> <p>Card2</p> </div>
 
-                          <div className="skills-card7">
-                              <p>Card3</p>
-                          </div>
+                          <div className="skills-card7"> <p>Card3</p> </div>
 
-                          <div className="skills-card8">
-                              <p>Card4</p>
-                          </div>
+                          <div className="skills-card8"> <p>Card4</p> </div>
 
-                          <div className="skills-card9">
-                              <p>Card1</p>
-                          </div>
+                          <div className="skills-card9"> <p>Card1</p> </div>
 
-                          <div className="skills-card10">
-                              <p>Card2</p>
-                          </div>
+                          <div className="skills-card10"> <p>Card2</p> </div>
 
-                          <div className="skills-card11">
-                              <p>Card3</p>
-                          </div>
+                          <div className="skills-card11"> <p>Card3</p> </div>
 
-                          <div className="skills-card12">
-                              <p>Card4</p>
-                          </div>
+                          <div className="skills-card12"> <p>Card4</p></div>
                        </div>
+
 
                        <div className="skills-content2">
-                           <div className="header-content2">
-                               <h3>this is working</h3>
-                           </div>
+                 
                         <div className="skills-content2-container">
-                            <ul>
-                                <li className="lorem5"><LoremIpsum p={1}/></li>
-                                <li className="lorem6"><LoremIpsum p={1}/></li>
-                                <li className="lorem7"><LoremIpsum p={1}/></li>
-                                <li className="lorem8"><LoremIpsum p={1}/></li>
-                                <li className="lorem9"><LoremIpsum p={1}/></li>
-                            </ul>
-                        </div>
-                         
-                       </div>
+                               <div className="skills-content2-header">
+                                    <h1>Services:</h1>
+                               </div>
+                                   <div className="skills-content2-cards">
+
+                                         <div className="skills-content2-card1">
+                                             <div className="content2-container">
+                                             <div className="skills-content2-card1-img"></div>
+                                             <div className="skills-content2-section">
+                                                 <h2>Responsive Design:</h2>
+                                             </div>
+                                             </div>
+                                         </div>
+
+
+                                         <div className="skills-content2-card2">
+                                             <div className="content2-container2">
+                                             <div className="skills-content2-card2-img"></div>
+                                             <div className="skills-content2-section2">
+                                                 <h2>Web Design: </h2>
+                                             </div>   
+                                            </div>
+                                         </div>
+
+
+                                         <div className="skills-content2-card3">
+                                             <div className="content2-container3">
+                                             <div className="skills-content2-card3-img"></div>
+                                             <div className="skills-content2-section3">
+                                                 <h2>Mobile Design:</h2>
+                                             </div>
+                                             </div>
+                                         </div>
+                                   </div>  
+                              </div>
+                         </div>
+
 
                        <div className="skills-content3">
-                           <div className="header-content3">
-                               <h3>this is working</h3>
-                           </div>
+            
                         <div className="skills-content3-container">
-                            <ul>
-                                <li className="lorem10"><LoremIpsum p={1}/></li>
-                                <li className="lorem11"><LoremIpsum p={1}/></li>
-                                <li className="lorem12"><LoremIpsum p={1}/></li>
-                                <li className="lorem13"><LoremIpsum p={1}/></li>
-                            </ul>
+                            <div className="skills-content3-header">
+                                <h1>Services:</h1>
+                            </div>
+                           <div className="skills-content3-cards">
+
+                             <div className="skills-content3-card1">
+                             <div className="skills-card1-header">
+                                     <h1>Soft Skills:</h1>
+                                 </div>
+                                 <ul>
+                                     <n/>
+                                     <li> <p>Leadership</p> </li>
+                                     <n/>
+                                     <li> <p>Organization</p> </li>
+                                     <n/>
+                                     <li> <p>Collaboration</p> </li>
+                                     <n/>
+                                     <li> <p>Wireframe</p> </li>
+                                     <n/>
+                                     <li> <p>Adobe[Photoshop, XD]</p> </li>
+                                     <n/>
+                                     <li> <p>Git</p> </li>
+                                     <n/>
+                                     <li> <p>Self Motivated</p> </li>
+                                 </ul>   
+                             </div>
+                             <div className="skills-content3-card2">
+                                 <div className="skills-card2-header">
+                                     <h1>Frontend:</h1>
+                                 </div>
+                                 <ul>
+                                     <n/>
+                                     <li> <p>Html</p> </li>
+                                     <n/>
+                                     <li> <p>Css</p> </li>
+                                     <n/>
+                                     <li> <p>Scss</p> </li>
+                                     <n/>
+                                     <li> <p>Javascript</p> </li>
+                                     <n/>
+                                     <li> <p>React</p> </li>
+                                     <n/>
+                                     <li> <p>Angular</p> </li>
+                                     <n/>
+                                     <li> <p>Web Pack</p> </li>
+                                 </ul>
+                             </div>
+                             <div className="skills-content3-card3">
+                             <div className="skills-card3-header">
+                                     <h1>Backend:</h1>
+                                 </div>
+                                  <ul>
+                                     <n/>
+                                     <li> <p>Php</p> </li>
+                                     <n/>
+                                     <li> <p>MySQL</p> </li>
+                                     <n/>
+                                     <li> <p>NodeJs</p> </li>
+                                     <n/>
+                                     <li> <p>Express</p> </li>
+                                     <n/>
+                                     <li> <p>Bcrypt</p> </li>
+                                     <n/>
+                                     <li> <p>Java</p> </li>
+                                     <n/>
+                                     <li> <p>MongoDB</p> </li>
+                                 </ul>
+                             </div>
+
+                         </div>
                         </div>
                        </div>
 
